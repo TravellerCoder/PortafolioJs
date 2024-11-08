b765d07c9e942f15fda389ffa758a24fbbf53c59
Traveller Coder <guillermobukowski@gmail.com>
2024-11-08 14:44:32 -0300

    config de scritp de menu desplegable de navbar para mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
+/*--------------- toogle navbar ---------------*/
+
+let menuIcon = document.querySelector('#menu-icon');
+let navBar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navBar.classList.toggle('active');
+}
+
+/*--------------- class active navbar and scroll to section ---------------*/
+
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
