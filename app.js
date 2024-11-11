18edbaa7baece6dded7de404d0412d8b46be12a8
Traveller Coder <guillermobukowski@gmail.com>
2024-11-11 09:53:06 -0300

    creacion de entry point

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const publicPaht = path.resolve(__dirname, 'assets');
+app.use(express.static(publicPath));
+//const port = process.env.PORT || 3000;
+const port = 3000;
+app.listen(port, (error) => {
+    if (error) {
+        return console.log(error)
+    }
+    console.log('server UUUUP')
+});
+
+
+app.get('/', function (req, res) {
+    res.sendFile(path.resolve(__dirname, './index.html'))
+});
+
+
+
